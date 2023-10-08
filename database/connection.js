10677e216bae3f70c847e560084ca291541ab01a
pentiumKing <ramahawai87@gmail.com>
2023-10-07 20:44:57 -0400

    initial commit

---
+++ database/connection.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+})
+
+mongoose.connection
+.on("open", ()=>console.log('connected to mongoDB'))
+.on('close', ()=>console.log('disconnected fro mongoDB'))
+on("error", (e) => console.log('error occured', e));
+module.exports = mongoose
