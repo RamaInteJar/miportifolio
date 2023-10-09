4aec056a3a141c30c159f9d20a5c260158fbb4f2
pentiumKing <ramahawai87@gmail.com>
2023-10-08 21:14:19 -0400

    crud operation

--- controllers/portifolioController.js
+++ controllers/portifolioController.js
@@ -29,4 +29,24 @@ router.get("/:id", async (req, res) => {
     res.status(400).json(error);
   }
 });
+//Delete
+router.delete("/:id", async (req, res) => {
+  try {
+    res.json(await Portifolio.findByIdAndDelete(req.params.id));
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+//Update
+router.put("/:id", async (req, res) => {
+  try {
+    res.json(
+      await Portifolio.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    );
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
 module.exports = router;
